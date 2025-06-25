f573e636e1167b2c81e1cdf50de510e20e8b324c
korn55 <6620301001@cdti.ac.th>
2025-06-25 11:23:03 +0700

    asycio05_up

--- assignment04/asyncio05.py
+++ assignment04/asyncio05.py
@@ -1,1 +1,14 @@
 # Check if a Task is Done
+import asyncio
+
+async def simple_task():
+    await asyncio.sleep(1)
+    return "เสร็จแล้ว"
+
+async def main():
+    task = asyncio.create_task(simple_task())
+    print("ก่อน await:", task.done()) # ยังไม่เสร็จ
+    await task
+    print("หลัง await:", task.done()) # เสร็จแล้ว
+
+asyncio.run(main())
