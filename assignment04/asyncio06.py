59477954110ed8d96e5c3a748d2d1b31bab7a489
korn55 <6620301001@cdti.ac.th>
2025-06-25 11:27:58 +0700

    asyncio_06_up

--- assignment04/asyncio06.py
+++ assignment04/asyncio06.py
@@ -1,1 +1,16 @@
 # check_cancelled.py
+import asyncio
+
+async def slow_task():
+    await asyncio.sleep(5)
+
+async def main():
+    task = asyncio.create_task(slow_task())
+    await asyncio.sleep(1)
+    task.cancel()
+    try:
+        await task
+    except asyncio.CancelledError:
+        print("Task ถูกยกเลิก:", task.cancelled()) # True 
+
+asyncio.run(main())
