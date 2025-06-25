defa03d69894a1b0b1d630774a76009c17eebf08
korn55 <6620301001@cdti.ac.th>
2025-06-25 10:57:06 +0700

    asy03_up

--- assignment04/asyncio02.py
+++ assignment04/asyncio02.py
@@ -1,1 +1,13 @@
 # Create 1 Task with High-Level API
+import asyncio
+
+async def do_something():
+    print("เริ่มทํางาน...")
+    await asyncio.sleep(2)
+    print("ทํางานเสร็จแล้ว!")
+
+async def main():
+    task = asyncio.create_task(do_something())
+    await task # รอให้ task เสร็จ
+
+asyncio.run(main())
--- assignment04/asyncio03.py
+++ assignment04/asyncio03.py
@@ -1,2 +1,20 @@
 # Create 2 Tasks with High-Level API
+import asyncio
 
+async def download_image(name, delay):
+    print(f"{name}กําลังโหลด" )
+    await asyncio.sleep(delay)
+    print(f"{name}โหลดเสร็จแล้ว")
+
+async def main():
+    # สร้าง 2 tasks พร้อมกัน
+    task1  = asyncio.create_task(download_image("ภาพที่ 1", 2))
+    task2  = asyncio.create_task(download_image("ภาพที่ 2", 3))
+
+
+    #รอให้ทั้งสอง task เสร็จ
+    await task1
+    await task2
+
+asyncio.run(main())
+    
