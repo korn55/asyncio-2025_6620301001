361d5abc04155b81b34fe2687710dc37769254f7
korn55 <6620301001@cdti.ac.th>
2025-06-25 10:16:06 +0700

    asycio01-03

--- assignment03/asyncio02.py
+++ assignment03/asyncio02.py
@@ -1,2 +1,14 @@
 # example of running a coroutine
+import asyncio
+# define a coroutine
+async def custom_coro():
+    # await another coroutine  (sleep 1 วินาที)
+    await asyncio.sleep(1)
 
+# coroutine หลัก
+async def main():
+    # execute my  custom coroutine
+    await custom_coro()
+
+# start the coroutine program
+asyncio.run(main())
