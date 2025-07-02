8814269654a4d4cb65eb3e72de312db347c04dc4
korn55 <6620301001@cdti.ac.th>
2025-07-02 11:12:09 +0700

    commit_asy3-4

--- assignment05/asyncio03.py
+++ assignment05/asyncio03.py
@@ -1,1 +1,29 @@
 # example of waiting for the first task to fail
+from random import random
+import asyncio
+
+# corotine to execute in new task
+async def task_coro(arg):
+    # generate a random value between 0 and 1
+    value = random()
+    await asyncio.sleep(value)
+    # report the value
+    print(f'>task {arg} done with {value}')
+    # conditionnally fail
+    if value < 0.1:
+        raise Exception(f'Something bad happened in {arg}')
+    
+# main coroutine 
+async def main():
+    # create many tasks
+    tasks = [asyncio.create_task(task_coro(i)) for i in range(10)]
+    # wait for the first task to fail, or all tasks to complete
+    done,pending = await asyncio.wait(tasks, return_when=asyncio.FIRST_EXCEPTION)
+    # report result
+    print('Done')
+    # get the first task to fail
+    first = done.pop
+    print(first)
+
+# start the asyncio program
+asyncio.run(main())
--- assignment05/asyncio04.py
+++ assignment05/asyncio04.py
@@ -1,1 +1,24 @@
 # example of waiting for all tasks to be completed with a timeout
+from random import random
+import asyncio
+
+# corooutine to execute in a new task
+async def task_coro(arg):
+    # generate a random value between 0 and 10
+    value = random()
+    # block for a moment
+    await asyncio.sleep(value)
+    # report the value 
+    print(f'>task {arg} done with {value}')
+    return f"task {arg} with {value}"
+
+# main corotine 
+async def main():
+    # create many tasks
+    tasks = [asyncio.create_task(task_coro(i)) for i in range(10)]
+    # wait for all task to complete 
+    done,pending = await asyncio.wait(tasks, timeout=0.5)
+    # report results
+    print(f'Done,{len(done)} task completed in time')
+# start the asyncio program
+asyncio.run(main())
